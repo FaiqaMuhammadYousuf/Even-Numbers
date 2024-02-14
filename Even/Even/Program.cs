d59ba9ad2862794fb353017048fd7ce9709a9c31
FaiqaMuhammadYousuf <faiqamuhammadyousuf@gmail.com>
2024-02-14 06:33:35 -0800

    Even Numbers

---
+++ Even/Even/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Even Numbers
+
+            Console.WriteLine("Even Numbers");
+            for (int i = 2; i <= 10; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
